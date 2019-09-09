cba480027400c1e73b9e9887927dd78160d209e2
Michaël Perrin <contact@michaelperrin.fr>
2019-09-08 20:47:19 -0500

    Initialize Redux

---
+++ photoshare-frontend/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import pictures from './pictures'
+
+export default combineReducers({
+  pictures,
+});
---
+++ photoshare-frontend/src/reducers/pictures.js
@@ -0,0 +1,8 @@
+const pictures = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+};
+
+export default pictures;
